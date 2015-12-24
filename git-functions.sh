09d5310bf051fee72253e6d5fc60ffd0d0be139c
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-24 12:57:32 +0900

    Add git-delete-merged

--- git-functions.sh
+++ git-functions.sh
@@ -7,3 +7,22 @@ if [ -f ~/dotfiles/git-completion.bash ]; then
 else
   export PS1='\[\033[01;32m\][\u@\h \t]\[\033[01;33m\] \w \n\[\033[01;34m\]\$\[\033[00m\] '
 fi
+
+function git-delete-merged() {
+  merged=`git branch --merged | egrep -v '\*|develop|master'`
+  if [[ $merged = "" ]]; then
+    echo 'No merged branch.'
+    return 0
+  fi
+
+  echo -en "$merged\\ndelete? (y)es/(n)o: "
+  read ans
+
+  if [[ $ans =~ ^y(es)?$ ]]; then
+    echo "$merged" | xargs git branch -d
+  else
+    echo "cancelled."
+  fi
+}
+
+alias gdm='git-delete-merged'

6ad785b258101a88fa30f7a6f8156cc559a7e6b9
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-11-08 16:39:41 +0900

    Add git configs to links

--- create_links.sh
+++ create_links.sh
@@ -4,3 +4,5 @@ ln -s dotfiles/.vimrc .vimrc
 ln -s dotfiles/.vim/ .vim
 ln -s dotfiles/.agignore .agignore
 ln -s dotfiles/.bash_profile .bash_profile
+ln -s dotfiles/gitignore .gitignore
+ln -s dotfiles/gitconfig .gitconfig

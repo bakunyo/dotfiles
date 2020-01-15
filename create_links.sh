85000fdb4aecbc3e4c91a6b2c4d000beefaea7a3
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2020-01-15 15:39:23 +0900

    Change create_links to idempotent

--- create_links.sh
+++ create_links.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+rm .vimrc
 ln -s dotfiles/.vimrc .vimrc
-ln -s dotfiles/.vim/ .vim
+
+rm -rf .vim
+ln -s dotfiles/.vim .vim
+
+rm .agignore
 ln -s dotfiles/.agignore .agignore
+
+rm .bash_profile
 ln -s dotfiles/.bash_profile .bash_profile
+
+rm .gitignore
 ln -s dotfiles/gitignore .gitignore
+
+rm .gitconfig
 ln -s dotfiles/gitconfig .gitconfig
